2d6b0ecd975e3b39cf1aa8cbb0ce14aa4e62780c
Conor Maguire <conormag94@gmail.com>
2017-11-08 18:06:04 +0000

    Init file service

---
+++ file_service/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+sample_files = [
+        {
+            'id': 1,
+            'name': 'hello.txt',
+            'content': 'This is a sample txt file'
+        },
+        {
+            'id': 2,
+            'name': 'foo.txt',
+            'content': 'I\'m another file'
+        },
+        {
+            'id': 3,
+            'name': 'bar.txt',
+            'content': 'A third file to test updating'
+        }
+]
+
+@app.route('/files', methods=['GET'])
+def files_index():
+    return jsonify({'files': sample_files})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
