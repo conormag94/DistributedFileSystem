929ffef7098bb158ea8fd165d6a5a181764199dd
Conor Maguire <conormag94@gmail.com>
2017-11-24 19:46:05 +0000

    Add missing import in common

--- common/common/__init__.py
+++ common/common/__init__.py
@@ -1,3 +1,10 @@
+import requests
+
+FILE_SERVER_HOST = '192.168.99.100'
+FILE_SERVER_PORT = 5001
+
+FILE_SERVER = f'http://{FILE_SERVER_HOST}:{FILE_SERVER_PORT}/files'
+
 def list_files():
     response = requests.get(FILE_SERVER).json()
     file_list = response['data']['files']
