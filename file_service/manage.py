ef78d97e3712a9db406c18eb698232e8b6de48e7
Conor Maguire <conormag94@gmail.com>
2017-11-24 15:14:15 +0000

    Add seed_db command

--- file_service/manage.py
+++ file_service/manage.py
@@ -8,8 +8,14 @@ manager = Manager(app)
 
 @manager.command
 def recreate_db():
+    """Recreate a new empty database"""
     db.drop_all()
     db.create_all()
+    db.session.commit()
+
+@manager.command
+def seed_db():
+    db.session.add(File(filename="init1.txt", content="Seeding the database"))
     db.session.commit()
 
 if __name__ == '__main__':
