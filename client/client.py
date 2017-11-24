d7371408e07e902f072c073040ff112f6727b20e
Conor Maguire <conormag94@gmail.com>
2017-11-24 15:54:16 +0000

    Basic communication between client and file server

--- client/client.py
+++ client/client.py
@@ -3,16 +3,31 @@ import requests
 import sys
 
 
-def greeting():
-    print('===== Distributed File System Client =====')
+prompt = '\033[1;33m' + '> ' + '\033[0;0m'
+
+FILE_SERVER_HOST = sys.argv[1]
+FILE_SERVER_PORT = 5001
+
+FILE_SERVER = f'http://{FILE_SERVER_HOST}:{FILE_SERVER_PORT}/files'
+
+def list_files():
+    response = requests.get(FILE_SERVER).json()
+    file_list = response['data']['files']
+    for file in file_list:
+        print(file['filename'], '\t', file['created_at'])
 
 def main():
     while(True):
-        inp = input('> ')
-        if inp == 'quit':
+        cmd = input(prompt)
+        if cmd == 'q':
             sys.exit(1)
+        if cmd == 'list':
+            list_files()
         else:
-            print(inp)
+            print(cmd)
+
+def greeting():
+    print('===== Distributed File System Client =====')
 
 if __name__ == '__main__':
     greeting()
