9d96edd2405787fe74ab07d6dcfd750798600f06
Conor Maguire <conormag94@gmail.com>
2017-11-30 19:29:54 +0000

    Add get and list to client

--- client/client.py
+++ client/client.py
@@ -1,29 +1,55 @@
 import os
 import sys
 
-from common import list_files
+
+import requests
 
 
-prompt = '\033[1;33m' + '> ' + '\033[0;0m'
+prompt = '\033[1;32m' + '> ' + '\033[0;0m'
 
+DOWNLOADS_LOCATION = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'user_files')
+FILE_SERVER_HOST = 'http://192.168.99.100'
+
+FILE_SERVER_URLS = {
+    "1": f"{FILE_SERVER_HOST}:5001/files",
+    "2": f"{FILE_SERVER_HOST}:5002/files"
+}
+
+def list_files(file_server):
+    url = FILE_SERVER_URLS[file_server]
+    r = requests.get(url).json()
+    return r['files']
+
+def get_file(filename, file_server):
+    url = FILE_SERVER_URLS[file_server] + '/' + filename
+    r = requests.get(url)
+    return r
 
 def main():
     while(True):
-        cmd = input(prompt)
-        if cmd == 'q':
+        cmd = input(prompt).split(' ')
+        if cmd[0] == 'q':
             sys.exit(1)
-        if cmd == 'list':
-            files = list_files()
+        elif cmd[0] == 'ls':
+            files = list_files(file_server="1")
             print(f'{len(files)} file(s) found:')
+            print("------------------")
             for file in files:
-                print(file['filename'], '\t', file['created_at'])
+                print(file)
+        elif cmd[0] == 'get':
+            filename = cmd[1]
+            response = get_file(filename, file_server="1")
+
+            save_path = os.path.join(DOWNLOADS_LOCATION, filename)
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
         else:
             print(cmd)
 
 def greeting():
     print('Commands:')
     print('---------')
-    print('list\tList files')
+    print('ls\tList files')
     print('q\tQuit')
 
 if __name__ == '__main__':
