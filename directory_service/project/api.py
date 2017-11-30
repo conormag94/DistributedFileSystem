5d75c1c021ce34c090f917238c15f485e6840842
Conor Maguire <conormag94@gmail.com>
2017-11-30 20:30:11 +0000

    Add file location endpoint to directory service

--- directory_service/project/api.py
+++ directory_service/project/api.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Blueprint, jsonify, request, send_from_directory
+from flask import Blueprint, jsonify, request
 from sqlalchemy import exc
 from werkzeug.utils import secure_filename
 
@@ -14,6 +14,18 @@ directory = Blueprint('directory', __name__)
 @directory.route('/', methods=['GET'])
 def index():
     return "Directory Server", 200
+
+@directory.route('/locate/<filename>', methods=['GET'])
+def locate_file(filename):
+    """TODO: Remove hardcoded file server location"""
+    
+    file_url = f'http://192.168.99.100:5001/files/{filename}'
+    response = {
+        "status": "success",
+        "file_location": file_url
+    }
+
+    return jsonify(response), 200
 
 
 
