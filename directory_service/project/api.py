0e958f35b9ee38c9c916670e949378020b04f689
Conor Maguire <conormag94@gmail.com>
2017-12-05 21:52:17 +0000

    Add list files endpoint to directory server

--- directory_service/project/api.py
+++ directory_service/project/api.py
@@ -1,5 +1,7 @@
 import os
 
+import requests
+
 from flask import Blueprint, jsonify, request
 from sqlalchemy import exc
 from werkzeug.utils import secure_filename
@@ -11,6 +13,14 @@ from flask import current_app as app
 
 directory = Blueprint('directory', __name__)
 
+file_server_1 = os.environ.get("FS1_URL")
+file_server_2 = os.environ.get("FS2_URL")
+
+file_servers = [
+    file_server_1,
+    file_server_2
+]
+
 @directory.route('/', methods=['GET'])
 def index():
     return "Directory Server", 200
@@ -19,13 +29,27 @@ def index():
 def locate_file(filename):
     """TODO: Remove hardcoded file server location"""
     
-    file_url = f'http://192.168.99.100:5001/files/{filename}'
+    file_url = file_servers[0]
     response = {
         "status": "success",
-        "file_location": file_url
+        "file_location": file_url,
     }
 
     return jsonify(response), 200
 
+@directory.route('/list', methods=['GET'])
+def list_files():
+
+    filenames = []
+    for server in file_servers:
+        r = requests.get(f"{server}/files")
+        data = r.json()
+        filenames += data["files"] 
+
+    response = {
+        "status": "success",
+        "files": filenames
+    }
+    return jsonify(response), 200
 
 
