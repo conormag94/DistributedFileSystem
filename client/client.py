2454efbf86f63fddf0fc36a73b2ca2a5c7c352c8
Conor Maguire <conormag94@gmail.com>
2017-12-08 15:50:35 +0000

    Add read action into client

--- client/client.py
+++ client/client.py
@@ -12,22 +12,47 @@ CACHE_LOCATION = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache
 DIRECTORY_SERVICE = "http://192.168.99.100:5000"
 
 def list_files():
+    """
+    Get list of all files from the directory service.
+    """
     url = f"{DIRECTORY_SERVICE}/files"
     r = requests.get(url).json()
     return r
 
 def get_file(filename):
+    """
+    Get a file from the directory service.
+    """
     url = f"{DIRECTORY_SERVICE}/files/{filename}"
     r = requests.get(url)
     return r
 
+def read_file(filename):
+    """
+    Print the contents of a locally cached file.
+    """
+    cached_file = os.path.join(CACHE_LOCATION, filename)
+    if not os.path.exists(cached_file):
+        print(f"{filename} not cached locally")
+        print(f"You may need to open it first with the \'open\' command")
+    else:
+        filesize = os.stat(cached_file).st_size
+        with open(cached_file, 'rb') as f:
+            print('\033[1;35m', end="")
+            print(f"  {filename}:")
+            print('\033[0;0m', end="")
+            print(f.read().decode())
+
 def greeting():
     print('Commands:')
     print('---------')
-    print('ls\tList files')
-    print('open <filename>\tDownload file from remote file system')
-    print('upload <filepath>\tUpload a file to the DFS')
-    print('q\tQuit')
+    print('  ls            - List files (remote and cached)')
+    print('  open <file>   - Download file from remote file system')
+    print('  read <file>   - Read contents of locally cached file (.txt files only)')
+    print('  upload <file> - Upload a file to the DFS')
+    print('  help          - Show this menu again')
+    print('  q             - Quit')
+    print('---------')
 
 def main():
     while(True):
@@ -61,7 +86,9 @@ def main():
             else:
                 error_msg = response.json()["message"]
                 print(f"\'{filename}\': {error_msg}")
-        
+        elif cmd[0] == 'read':
+            filename = cmd[1]
+            read_file(filename)
         elif cmd[0] == 'upload':
             filepath = os.path.expanduser(cmd[1])
             filename = os.path.basename(filepath)
@@ -73,7 +100,8 @@ def main():
                     r = requests.post(url, files={"user_file": (filename, f)})
                     print(r.status_code, "-", r.content)
             print("Upload not yet implemented")
-        
+        elif cmd[0] == 'help':
+            greeting()
         else:
             print("Error: Unrecognized command")
             greeting()
