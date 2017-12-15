de667498bb232033e8fbdadafcb327465068b40b
Conor Maguire <conormag94@gmail.com>
2017-12-15 22:25:08 +0000

    Add login and generate_token functions

--- security_service/project/api.py
+++ security_service/project/api.py
@@ -4,11 +4,29 @@ from flask import Blueprint, jsonify, request
 from sqlalchemy import exc
 
 from project import db
-from project.models import User
+from project.models import User, SessionToken
 
 from flask import current_app as app
 
 security = Blueprint('security', __name__)
+
+def generate_token(username, password):
+    if SessionToken.query.filter_by(username=username.lower()).first() is not None:
+        return jsonify({"status": "fail", "message": "User already logged in"}), 403
+
+    user = User.query.filter_by(username=username.lower()).first()
+    if not user:
+        return jsonify({"status": "fail", "message": "User not registered"}), 404
+    
+    if user.verify_password(password):
+        token = SessionToken(username=username)
+        db.session.add(token)
+        db.session.commit()
+        return jsonify({"status": "success", "token": token.to_hex}), 200
+
+    else:
+        return jsonify({"status": "fail", "message": "Incorrect password"}), 403
+
 
 @security.route('/', methods=['GET'])
 def index():
@@ -25,6 +43,20 @@ def list_users():
     response = {
         "status": "success",
         "users": users
+    }
+    return jsonify(response), 200
+
+@security.route('/tokens', methods=['GET'])
+def list_session_tokens():
+
+    tokens = []
+    tokenObjs = SessionToken.query.all()
+    for token in tokenObjs:
+        tokens.append(token.to_dict())
+
+    response = {
+        "status": "success",
+        "tokens": tokens
     }
     return jsonify(response), 200
 
@@ -48,3 +80,16 @@ def create_user():
         return jsonify({"status": "success", "user": username}), 200
     except:
         return jsonify({"status": "fail", "message": "Unable to create user"}), 500
+
+@security.route('/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"status": "fail", "message": "Bad request, need username and password"}), 403
+
+    response = generate_token(username, password)
+
+    return response
--- security_service/project/models.py
+++ security_service/project/models.py
@@ -1,3 +1,5 @@
+import secrets
+
 from project import db
 
 class User(db.Model):
@@ -10,9 +12,31 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+    def verify_password(self, password):
+        return self.password == password
+
     def to_dict(self):
         """Export user to dictionary data structure"""
         return {
             'id': self.id,
             'username': self.username
+        }
+
+class SessionToken(db.Model):
+    username = db.Column(db.String(128), primary_key=True, nullable=False)
+    token = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, username):
+        self.username = username
+        self.token = secrets.token_hex(16)
+
+    @property
+    def to_hex(self):
+        return self.token
+
+    def to_dict(self):
+        """Export user to dictionary data structure"""
+        return {
+            'username': self.username,
+            'token': self.token
         }
